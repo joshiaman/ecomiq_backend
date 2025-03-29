ddd4faf0529bdc8dbb7c6a50e0c2e437aa5aa7ff
joshiaman112 <joshiaman112@gmail.com>
2025-03-29 13:57:24 -0400

    Fixed Issues

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,6 +1,6 @@
 # config/initializers/paypal.rb
 PayPal::SDK.configure(
-  mode: ENV["PAYPAL_MODE"], # "sandbox" or "live"
-  client_id: ENV["PAYPAL_CLIENT_ID"],
-  client_secret: ENV["PAYPAL_CLIENT_SECRET"]
+  mode: 'sandbox', # "sandbox" or "live"
+  client_id: "AdxNKa1BCIwnicc9p1fCYGOH1bUYSbnDJNfFSkxt3HKhoEwgTrETaYWBP1lR9dG86NlLugIO8PsMMB0Z",
+  client_secret: "EKWHe0nt2A0Di_Dn0UlQAG4NEUc2bSEzHM7vKZu0cUqHLZCuHxAkvt9KwiDGBhx0Mkbr_LV81y6Kcyzt"
 )
